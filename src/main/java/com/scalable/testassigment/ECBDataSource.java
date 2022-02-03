ffe3cc0fa579b8372cb35faee4e0ef3781e7c9d8
Alexandra Dobryn (ext.) <Alexandra.Dobryn@partner.bmwgroup.com>
2022-02-03 19:23:00 +0100

    initialise rates list and get euro based rate for a currency

--- src/main/java/com/scalable/testassigment/ECBDataSource.java
+++ src/main/java/com/scalable/testassigment/ECBDataSource.java
@@ -1,11 +1,85 @@
 package com.scalable.testassigment;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.Map;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 @Component
 public class ECBDataSource {
-
     private Map<String, Double> euroBasedRatesMap;
+    private Map<String, Integer> currenciesUsagesMap;
+
+    ECBDataSource(){
+        euroBasedRatesMap = new HashMap<>();
+        currenciesUsagesMap = new HashMap<>();
+
+        //TODO: if initial rates download failed, exit with error and close app
+        initialiseRatesMap();
+    }
+
+    //TODO: make URL configurable
+    private static final String RATES_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ECBDataSource.class);
+
+    public Double getEuroBasedRate(String currency){
+        //TODO: better count those things as metrics
+        currenciesUsagesMap.put(currency, currenciesUsagesMap.get(currency) + 1);
+        return euroBasedRatesMap.get(currency);
+    }
+
+
+    //downloads rates from ECB and parses gotten XML to fill the map
+    public void initialiseRatesMap(){
+        Document doc = downloadRates();
+        doc.getDocumentElement().normalize();
+
+        NodeList list = doc.getElementsByTagName("Cube");
+        for(int i = 0; i < list.getLength(); i++){
+            Node node = list.item(i);
+
+            //we need only empty "cube" nodes here with currency and rate attributes
+            if (node.getNodeType() == Node.ELEMENT_NODE && !node.hasChildNodes()) {
+                Element element = (Element) node;
+                String currency = element.getAttribute("currency");
+                String rate = element.getAttribute("rate");
+                LOGGER.info("Read item currency: " + currency + " rate: "  + rate);
+
+                if(!currency.isEmpty() && !rate.isEmpty()){
+                    euroBasedRatesMap.put(currency, Double.parseDouble(rate));
+                    currenciesUsagesMap.put(currency, 0);
+                }
+            }
+        }
+    }
+
+    private Document downloadRates(){
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = dbf.newDocumentBuilder();
+
+            URL url = new URL(RATES_URL);
+            InputStream stream = url.openStream();
+
+            //TODO: add check that downloaded document is not empty
+            Document doc = docBuilder.parse(stream);
+            return doc;
+        } catch(Exception ex){
+            LOGGER.warn("Failed due to " + ex.getMessage());
+            return null;
+        }
+    }
+
 }
--- src/main/java/com/scalable/testassigment/ExchangeRateServiceController.java
+++ src/main/java/com/scalable/testassigment/ExchangeRateServiceController.java
@@ -1,20 +1,23 @@
 package com.scalable.testassigment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExchangeRateServiceController {
 
+    @Autowired
+    ECBDataSource dataSource;
+
     @GetMapping("exchangeRateService/v1/health")
     public String HealthCheck(){
         return "alive";
     }
 
-    @PostMapping("exchangeRateService/v1/getEuroBasedRate")
-    public String getEuroBasedExchangeRate(@RequestParam(value = "currency") String currency) {
-        return "a";
+    @GetMapping("exchangeRateService/v1/getEuroBasedRate")
+    public Double getEuroBasedExchangeRate(@RequestParam(value = "currency") String currency) {
+        return dataSource.getEuroBasedRate(currency);
     }
 }
